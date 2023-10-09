8540eab8f3fdc5438767654b78b3c2de5229f7ff
matheusrmello <matheus.rm.09@hotmail.com>
2023-10-09 19:02:12 -0300

    feature: Integrando a API com o front em React

--- routes/routes.go
+++ routes/routes.go
@@ -4,15 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/matheusrmello/go-rest-api/controllers"
+	"github.com/matheusrmello/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidade).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalidades/", controllers.CriaPersonalidade).Methods("POST")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonalidade).Methods("DELETE")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidades).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
 
